9ab752221a3fb75d04d4386a9f789f6c376569f1
stwer1998 <stwer1998@mail.ru>
2019-11-28 23:39:44 +0300

    Add api for download images

---
+++ ImageDownloader/Controllers/ImageController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using ImageDownloader.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageDownloader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private IHostingEnvironment appEnvironment;
+        public ImageController(IHostingEnvironment appEnvironment)
+        {
+            this.appEnvironment = appEnvironment;
+        }
+        [HttpGet]
+        public string Download()
+        {
+            return "Ok";
+        }
+
+        [HttpPost]
+        public string Download([FromQuery]string[] links)
+        {
+            string result = string.Empty;
+
+            foreach (var item in links)
+            {
+                result += DownLoadFile(item);
+            }
+
+            return result;
+        }
+
+
+        private string DownLoadFile(string src)
+        {
+            var link = new ImageModel(src);
+            string path = "/images/" + link.NameImage;
+            var a = appEnvironment.WebRootPath + "\\" + link.NameImage;
+            try
+            {
+                using (var wc = new WebClient() )
+                {
+                    wc.DownloadFile(link.Link, a);
+                }
+                return link.Link + " Downloaded  ";
+            }
+            catch (Exception e)
+            {
+                return link.Link + " Something wrong ";
+            }
+        }
+
+    }
+}
---
+++ ImageDownloader/Models/ImageModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageDownloader.Models
+{
+    public class ImageModel
+    {
+        public string Link { get; set; }
+
+        public string NameImage { get; set; }
+
+        public ImageModel(string link)
+        {
+            var url = new Uri(link);
+            Link = url.Scheme+ "://" + url.Host + url.PathAndQuery;
+            NameImage = url.Segments[url.Segments.Length - 1];
+            if (!NameImage.Contains('.')) 
+            {
+                NameImage += ".jpeg";
+            }
+        }
+    }
+}
